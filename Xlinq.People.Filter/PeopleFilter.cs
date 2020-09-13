c03241906fb373871b09d094e9471f4483e2d6f9
Gabor Benko <benkogabor@msn.com>
2020-09-13 20:26:45 +0200

    Basic filters with extension method

---
+++ Xlinq.People.Filter/PeopleFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using XlingPeople.Models.Models;
+
+namespace Xlinq.Filter
+{
+    public static class PeopleFilter
+    {
+        static int NumbrOfDepartment(this IEnumerable<People> peoples, string department)
+        {
+            return peoples.Count(x => x.Dept == department);
+        }
+
+        static IEnumerable<People> ToPaged(this IEnumerable<People> peoples, Func<People, bool> predicate, int page, int itemsinpage)
+        {
+
+            return peoples.Where(predicate).Skip((page - 1) * itemsinpage).Take(itemsinpage);
+        }
+    }
+}
